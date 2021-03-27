5b3dfaaf543953522cd362abe25bbb61e2e68e6c
Hitchnt <56658191+Hitchnt@users.noreply.github.com>
2021-03-27 19:52:20 -0400

    Revert "Timepicker dialog"

--- app/src/main/java/edu/ucandroid/weathernotice/dto/List.kt
+++
@@ -1,6 +0,0 @@
-package edu.ucandroid.weathernotice.dto
-
-import android.app.TimePickerDialog
-
-data class List(var city: String = "", var weather: String = "", var timePickerDialog: TimePickerDialog) {
-}
--- app/src/main/java/edu/ucandroid/weathernotice/fragments/ListViewModel.kt
+++
@@ -1,7 +0,0 @@
-package edu.ucandroid.weathernotice.fragments
-
-import androidx.lifecycle.ViewModel
-
-class ListViewModel : ViewModel() {
-
-}
